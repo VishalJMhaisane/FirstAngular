fc57e47eac65a078e31555569b826992050f04f8
VishalJMhaisane <vishal.mhaisane1994@gmail.com>
2017-03-23 09:07:47 -0700

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,31 @@
+
+<html>
+<head>
+<title>Login Here
+</title>
+<link rel="stylesheet" href="bootstrap.css">
+</head>
+
+
+<body>
+<?php
+include('Header.php');
+?>
+<center>
+<!-- <div class="panel panel-primary" style="width:45%">
+            <div class="panel-heading">
+              <h3 class="panel-title">Login Here </h3>
+            </div>
+            <div class="panel-body">-->
+<div>
+<h4>Welcome to Portal Of Bloom's Test</h4>
+<img src="">
+</div>
+            <!--</div>
+          </div>
+        </div><!-- /.col-sm-4 -->
+
+
+</center>
+</body>
+</html>
